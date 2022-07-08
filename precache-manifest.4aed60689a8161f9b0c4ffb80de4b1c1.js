f781204493f4cda9b55c33a142eb8273c0bbef62
SamuelLin213 <samuelddlin@gmail.com>
2022-07-07 21:41:34 -0700

    Updates

---
+++ precache-manifest.4aed60689a8161f9b0c4ffb80de4b1c1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d7d15169d282da44c791",
+    "url": "/react_info_site/static/css/main.84ac3b17.chunk.css"
+  },
+  {
+    "revision": "d7d15169d282da44c791",
+    "url": "/react_info_site/static/js/main.dc02c333.chunk.js"
+  },
+  {
+    "revision": "5b179f06042f528d2d83",
+    "url": "/react_info_site/static/js/runtime~main.e56a9711.js"
+  },
+  {
+    "revision": "492cbace2743ea5c9eda",
+    "url": "/react_info_site/static/js/2.ecd6ffc4.chunk.js"
+  },
+  {
+    "revision": "721a82dd756a4389231cd54eaafefb77",
+    "url": "/react_info_site/index.html"
+  }
+];
